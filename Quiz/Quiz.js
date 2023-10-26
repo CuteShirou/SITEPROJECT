df58c213ebde1a9cf2210ad1b9c59df55734845f
Valentin <111918150+CuteShirou@users.noreply.github.com>
2023-10-26 23:18:32 +0200

    Update

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -1,13 +1,9 @@
-// Questions will be asked
-
 let score = 0;
 const scoreElement = document.getElementById("score");
-
 function updateScore() {
   score++
   scoreElement.textContent = score;
 }
-
 const Questions = [{
   id: 0,
   q: "Quelle est la date de sortie de Beat Saber sur Windows ?",
@@ -100,44 +96,33 @@ const Questions = [{
   ]
 }
 ]
-
 // Set start
 var start = true;
-
 // Iterate
 function iterate(id) {
-
 // Getting the result display section
 var result = document.getElementsByClassName("result");
 result[0].innerText = "";
-
 // Getting the question
 const question = document.getElementById("question");
-
-
 // Setting the question text
 question.innerText = Questions[id].q;
-
 // Getting the options
 const op1 = document.getElementById('op1');
 const op2 = document.getElementById('op2');
 const op3 = document.getElementById('op3');
 const op4 = document.getElementById('op4');
-
 // Providing option text 
 op1.innerText = Questions[id].a[0].text;
 op2.innerText = Questions[id].a[1].text;
 op3.innerText = Questions[id].a[2].text;
 op4.innerText = Questions[id].a[3].text;
-
 // Providing the true or false value to the options
 op1.value = Questions[id].a[0].isCorrect;
 op2.value = Questions[id].a[1].isCorrect;
 op3.value = Questions[id].a[2].isCorrect;
 op4.value = Questions[id].a[3].isCorrect;
-
 var selected = "";
-
 // Show selection for op1
 op1.addEventListener("click", () => {
   op1.style.backgroundColor = "#027991";
@@ -146,7 +131,6 @@ op1.addEventListener("click", () => {
   op4.style.backgroundColor = "#813C58";
   selected = op1.value;
 })
-
 // Show selection for op2
 op2.addEventListener("click", () => {
   op1.style.backgroundColor = "#813C58";
@@ -155,7 +139,6 @@ op2.addEventListener("click", () => {
   op4.style.backgroundColor = "#813C58";
   selected = op2.value;
 })
-
 // Show selection for op3
 op3.addEventListener("click", () => {
   op1.style.backgroundColor = "#813C58";
@@ -164,7 +147,6 @@ op3.addEventListener("click", () => {
   op4.style.backgroundColor = "#813C58";
   selected = op3.value;
 })
-
 // Show selection for op4
 op4.addEventListener("click", () => {
   op1.style.backgroundColor = "#813C58";
@@ -173,10 +155,8 @@ op4.addEventListener("click", () => {
   op4.style.backgroundColor = "#027991";
   selected = op4.value;
 })
-
 // Grabbing the evaluate button
 const evaluate = document.getElementsByClassName("evaluate");
-
 // Evaluate method
 evaluate[score].addEventListener("click", () => {
   if (selected == "true") {
@@ -190,15 +170,12 @@ evaluate[score].addEventListener("click", () => {
   evaluate.disabled = false
 })
 }
-
 if (start) {
 iterate("0");
 }
-
 // Next button and method
 const next = document.getElementsByClassName('next')[0];
 var id = 0;
-
 next.addEventListener("click", () => {
 start = false;
 if (id < 10) {
