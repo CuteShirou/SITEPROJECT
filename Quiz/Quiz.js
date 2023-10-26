08ae84466f695eaf68b25b73acffc8795fe4b390
Valentin <111918150+CuteShirou@users.noreply.github.com>
2023-10-26 11:57:09 +0200

    Update 1.25

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -1,381 +1,210 @@
-//
-// lib/lib.js
-//
-var Question = function (questionObj) {
-    this.value = {
-      text: "Question",
-      answers: []
-    };
-  
-    this.selectedAnswer = null;
-    this.html = null;
-    this.questionText = null;
-    this.questionAnswers = null;
-    this.questionFeedback = null;
-  
-    this.value = Object.assign(this.value, questionObj);
-  
-    this.onQuestionAnswered = ({ detail }) => {
-      this.selectedAnswer = {
-        value: detail.answer,
-        html: detail.answerHtml
-      };
-      this.update();
-  
-      document.dispatchEvent(
-        new CustomEvent("question-answered", {
-          detail: {
-            question: this,
-            answer: detail.answer
-          }
-        })
-      );
-    };
-  
-    this.create = function () {
-      this.html = document.createElement("div");
-      this.html.classList.add("question");
-  
-      this.questionText = document.createElement("h2");
-      this.questionText.textContent = this.value.text;
-  
-      this.questionAnswers = document.createElement("div");
-      this.questionAnswers.classList.add("answers");
-  
-      for (let i = 0; i < this.value.answers.length; i++) {
-        const ansObj = this.value.answers[i];
-        let answer = createAnswer(ansObj);
-  
-        answer.onclick = (ev) => {
-          if (this.selectedAnswer !== null) {
-            this.selectedAnswer.html.classList.remove("selected");
-          }
-  
-          answer.classList.add("selected");
-  
-          this.html.dispatchEvent(
-            new CustomEvent("question-answered", {
-              detail: {
-                answer: ansObj,
-                answerHtml: answer
-              }
-            })
-          );
-        };
-  
-        this.questionAnswers.appendChild(answer);
-      }
-  
-      this.questionFeedback = document.createElement("div");
-      this.questionFeedback.classList.add("question-feedback");
-  
-      this.html.appendChild(this.questionText);
-      this.html.appendChild(this.questionAnswers);
-      this.html.appendChild(this.questionFeedback);
-  
-      this.html.addEventListener("question-answered", this.onQuestionAnswered);
-  
-      return this.html;
-    };
-  
-    this.disable = function () {
-      this.html.classList.add("disabled");
-      this.html.onclick = (ev) => {
-        ev.stopPropagation();
-      };
-  
-      this.html.removeEventListener("question-answered", this.onQuestionAnswered);
-  
-      let answers = this.html.querySelectorAll(".answer");
-      for (let i = 0; i < answers.length; i++) {
-        let answer = answers[i];
-        answer.onclick = null;
-      }
-    };
-  
-    this.remove = function () {
-      let children = this.html.querySelectorAll("*");
-      for (let i = 0; i < children.length; i++) {
-        const child = children[i];
-        this.html.removeChild(child);
-      }
-  
-      this.html.removeEventListener("question-answered", this.onQuestionAnswered);
-  
-      this.html.parentNode.removeChild(this.html);
-      this.html = null;
-    };
-  
-    this.update = function () {
-      let correctFeedback, incorrectFeedback;
-      this.html = this.html || document.createElement("div");
-  
-      correctFeedback = "Nice! You got it right.";
-      incorrectFeedback = "Oh! Not the correct answer.";
-  
-      if (this.selectedAnswer !== null) {
-        if (this.selectedAnswer.value.isCorrect) {
-          this.html.classList.add("correct");
-          this.html.classList.remove("incorrect");
-          this.questionFeedback.innerHTML = correctFeedback;
-        } else {
-          this.html.classList.add("incorrect");
-          this.html.classList.remove("correct");
-          this.questionFeedback.innerHTML = incorrectFeedback;
-        }
-      }
-    };
-  
-    function createAnswer(obj) {
-      this.value = {
-        text: "Answer",
-        isCorrect: false
-      };
-  
-      this.value = Object.assign(this.value, obj);
-  
-      this.html = document.createElement("button");
-      this.html.classList.add("answer");
-  
-      this.html.textContent = this.value.text;
-  
-      return this.html;
-    }
-  };
-  
-  //
-  // main.js
-  //
-  
-  let questionsData = [
-    {
-      text: "Quelle est la date de création de Beat Saber sur Windows ?",
-      answers: [
-        {
-          text: "2016",
-          isCorrect: false
-        },
-        {
-          text: "2017",
-          isCorrect: false
-        },
-        {
-          text: "2018",
-          isCorrect: false
-        },
-        {
-          text: "2019",
-          isCorrect: true
-        }
-      ]
-    },
-    {
-      text: "Quelle est le premier groupe de musique a avoir fais un partenariat avec Beat Saber ?",
-      answers: [
-        {
-          text: "Imagine Dragon",
-          isCorrect: true
-        },
-        {
-          text: "K/DA",
-          isCorrect: false
-        },
-        {
-          text: "Linkin Park",
-          isCorrect: false
-        },
-        {
-          text: "Lady Gaga",
-          isCorrect: false
-        }
-      ]
-    },
-    {
-      text: "Quelle est la plus grande difficulté disponible dans le jeux ?",
-      answers: [
-        {
-          text: "Insane",
-          isCorrect: false
-        },
-        {
-          text: "Master",
-          isCorrect: false
-        },
-        {
-          text: "Expert+",
-          isCorrect: true
-        }
-      ]
-    },
-    {
-      text: "Es ce que le jeu peut avoir des mods ?",
-      answers: [
-        {
-          text: "Oui",
-          isCorrect: true
-        },
-        {
-          text: "Non",
-          isCorrect: false
-        },
-      ]
-    },
-    {
-      text: "Quel studio developpe Beat Saber?",
-      answers: [
-        {
-          text: "Beat Games",
-          isCorrect: true
-        },
-        {
-          text: "7th Beat Studio",
-          isCorrect: false
-        },
-        {
-          text: "Super Spin Digital",
-          isCorrect: false
-        },
-        {
-          text: "PeroPero Inc.",
-          isCorrect: false
-        }
-      ]
-    },
-    {
-      text: "Quel est le moteur graphique de Unity",
-      answers: [
-        {
-          text: "Unreal Engine 4",
-          isCorrect: false
-        },
-        {
-          text: "Unreal Engine 5",
-          isCorrect: false
-        },
-        {
-          text: "Unity",
-          isCorrect: true
-        },
-        {
-          text: "Unreal Unity Engine 6",
-          isCorrect: false
-        }
-      ]
-    },
-    {
-      text: "Quelle est la chanson considerer comme la plus dur de Beat Saber?",
-      answers: [
-        {
-          text: "100$ Bill: Jaroslav Beck",
-          isCorrect: false
-        },
-        {
-          text: "Final Boss: Camellia",
-          isCorrect: false
-        },
-        {
-          text: "Overkill: Riot",
-          isCorrect: false
-        },
-        {
-          text: "Ghost: Camellia",
-          isCorrect: true
-        }
-      ]
-    },
-    {
-      text: "Quelle est la meilleur coupe possible pour faire un maximum de points ?",
-      answers: [
-        {
-          text: "idéalement 100°",
-          isCorrect: true
-        },
-        {
-          text: "idéalement 60°",
-          isCorrect: false
-        },
-      ]
-    },
-    {
-      text: "Quelle est la plus grande note possible ?",
-      answers: [
-        {
-          text: "Perfect",
-          isCorrect: false
-        },
-        {
-          text: "S",
-          isCorrect: false
-        },
-        {
-          text: "S+",
-          isCorrect: false
-        },
-        {
-          text: "SS",
-          isCorrect: true
-        },
-      ]
-    },
-    {
-      text: "Quelle est la chanson la plus longue de Beat Saber? (dans le jeu de base)",
-      answers: [
-        {
-          text: "Seven Nation Army: The White Stripes",
-          isCorrect: false
-        },
-        {
-          text: "Smells Like Teen Spirit: Nirvana",
-          isCorrect: false
-        },
-        {
-          text: "Free Bird: Lynyrd Skynyrd",
-          isCorrect: true
-        },
-        {
-          text: "I was made for Lovin' You: KISS",
-          isCorrect: false
-        }
-      ]
-    },
-  ];
-  
-  // variables initialization
-  let questions = [];
-  let score = 0,
-    answeredQuestions = 0;
-  let appContainer = document.getElementById("questions-container");
-  let scoreContainer = document.getElementById("score-container");
-  scoreContainer.innerHTML = `Score: ${score}/${questionsData.length}`;
+// Questions will be asked
 
-  // creating questions
-  for (var i = 0; i < 10; i++) {
-    let question = new Question({
-      text: questionsData[i].text,
-      answers: questionsData[i].answers
-    });
-  
-    appContainer.appendChild(question.create());
-    questions.push(question);
-    document.getElementById("app-container").style.visibility = "visible"; 
-    questionsData.style.visibility
+let score = 0;
+const scoreElement = document.getElementById("score");
+
+function updateScore() {
+  score++
+  scoreElement.textContent = score;
+}
+
+const Questions = [{
+  id: 0,
+  q: "Quelle est la date de création de Beat Saber sur Windows ?",
+  a: [{ text: "2016", isCorrect: false },
+      { text: "2017", isCorrect: false },
+      { text: "2018", isCorrect: false },
+      { text: "2019", isCorrect: true }
+  ]
+
+},
+{
+  id: 1,
+  q: "Quelle est le premier groupe de musique a avoir fais un partenariat avec Beat Saber ?",
+  a: [{ text: "Imagine Dragons", isCorrect: true, isSelected: false },
+      { text: "K/DA", isCorrect: false },
+      { text: "Linkin Park", isCorrect: false },
+      { text: "Lady Gaga", isCorrect: false }
+  ]
+},
+{
+  id: 2,
+  q: "Quelle est la plus grande difficulté disponible dans le jeux ?",
+  a: [{ text: "Insane", isCorrect: false },
+      { text: "Master", isCorrect: false },
+      { text: "Expert+", isCorrect: true },
+      { text: ".", isCorrect: false }
+  ]
+},
+{
+  id: 3,
+  q: "Beat Saber supporte les mods.",
+  a: [{ text: "Vrai", isCorrect: true },
+      { text: "Faux", isCorrect: false },
+      { text: "", isCorrect: false },
+      { text: "", isCorrect: false }
+  ]
+},
+{
+  id: 4,
+  q: "Quel studio developpe Beat Saber?",
+  a: [{ text: "Beat Games", isCorrect: true },
+      { text: "7th Beat Studio", isCorrect: false },
+      { text: "Super Spin Digital", isCorrect: false },
+      { text: "PeroPero Inc.", isCorrect: false }
+  ]
+},
+{
+  id: 5,
+  q: "Quel est le moteur graphique de Beat Saber ?",
+  a: [{ text: "Unreal Engine 4", isCorrect: false },
+      { text: "Unreal Engine 5", isCorrect: false },
+      { text: "Unity", isCorrect: true },
+      { text: "Unreal Unity engine 6", isCorrect: false }
+  ]
+},
+{
+  id: 6,
+  q: "Quelle est la chanson considerer comme la plus dur de Beat Saber?",
+  a: [{ text: "100$ Bill: Jaroslav Beck", isCorrect: false },
+      { text: "Final Boss: Camellia", isCorrect: false },
+      { text: "Overkill: Riot", isCorrect: false },
+      { text: "Ghost: Camellia", isCorrect: true }
+  ]
+},
+{
+  id: 7,
+  q: "Quelle est la meilleur coupe possible pour faire un maximum de points ?",
+  a: [{ text: "100°", isCorrect: true },
+      { text: "60°", isCorrect: false },
+      { text: "", isCorrect: false },
+      { text: "", isCorrect: false }
+  ]
+},
+{
+  id: 8,
+  q: "Quelle est la plus grande note possible ?",
+  a: [{ text: "Perfect", isCorrect: false },
+      { text: "S", isCorrect: false },
+      { text: "S+", isCorrect: false },
+      { text: "SS", isCorrect: true }
+  ]
+},
+{
+  id: 9,
+  q: "Quelle est la chanson la plus longue de Beat Saber? (dans le jeu de base)",
+  a: [{ text: "Seven Nation Army: The White Stripes", isCorrect: false },
+      { text: "Smells like teen spirit: Nirvana", isCorrect: false },
+      { text: "Free Bird: Lynyrd Skynyrd", isCorrect: true },
+      { text: "I was made for Lovin' you: KISS", isCorrect: false }
+  ]
+}
+]
+
+// Set start
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+// Getting the result display section
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+// Getting the question
+const question = document.getElementById("question");
+
+
+// Setting the question text
+question.innerText = Questions[id].q;
+
+// Getting the options
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
+
+
+// Providing option text 
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+// Providing the true or false value to the options
+op1.value = Questions[id].a[0].isCorrect;
+op2.value = Questions[id].a[1].isCorrect;
+op3.value = Questions[id].a[2].isCorrect;
+op4.value = Questions[id].a[3].isCorrect;
+
+var selected = "";
+
+// Show selection for op1
+op1.addEventListener("click", () => {
+  op1.style.backgroundColor = "#027991";
+  op2.style.backgroundColor = "#813C58";
+  op3.style.backgroundColor = "#813C58";
+  op4.style.backgroundColor = "#813C58";
+  selected = op1.value;
+})
+
+// Show selection for op2
+op2.addEventListener("click", () => {
+  op1.style.backgroundColor = "#813C58";
+  op2.style.backgroundColor = "#027991";
+  op3.style.backgroundColor = "#813C58";
+  op4.style.backgroundColor = "#813C58";
+  selected = op2.value;
+})
+
+// Show selection for op3
+op3.addEventListener("click", () => {
+  op1.style.backgroundColor = "#813C58";
+  op2.style.backgroundColor = "#813C58";
+  op3.style.backgroundColor = "#027991";
+  op4.style.backgroundColor = "#813C58";
+  selected = op3.value;
+})
+
+// Show selection for op4
+op4.addEventListener("click", () => {
+  op1.style.backgroundColor = "#813C58";
+  op2.style.backgroundColor = "#813C58";
+  op3.style.backgroundColor = "#813C58";
+  op4.style.backgroundColor = "#027991";
+  selected = op4.value;
+})
+
+// Grabbing the evaluate button
+const evaluate = document.getElementsByClassName("evaluate");
+
+// Evaluate method
+evaluate[score].addEventListener("click", () => {
+  if (selected == "true") {
+      result[0].innerHTML = "True";
+      result[0].style.color = "#309eff";
+      updateScore()
+  } else {
+      result[0].innerHTML = "False";
+      result[0].style.color = "#f03030";
   }
+  evaluate.disabled = true
+})
+}
 
-  document.addEventListener("question-answered", ({ detail }) => {
-    if (detail.answer.isCorrect) {
-      score++;
-    }
-  
-    answeredQuestions++;
-    scoreContainer.innerHTML = `Score: ${score}/${questions.length}`;
-    detail.question.disable();
-  
-    if (answeredQuestions == questions.length) {
-      setTimeout(function () {
-        alert(`Quiz completed! \nFinal score: ${score}/${questions.length}`);
-      }, 100);
-    }
-  });
-  
-  console.log(questions, questionsData);
-  
+if (start) {
+iterate("0");
+}
+
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 10) {
+  id++;
+  iterate(id);
+  console.log(id);
+}}
+)
